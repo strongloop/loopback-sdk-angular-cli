982d4292f95994cf8c49da4e34b8bfd9023a1837
Jan Hapke <jan.hapke@dreamlines.de>
2016-07-05 17:57:43 +0200

    added '-s' flag to pass the includeSchema option to loopback-sdk-angular

--- test/fixtures/app.js
+++ test/fixtures/app.js
@@ -8,4 +8,34 @@ var app = loopback();
 
 app.set('restApiRoot', '/rest-api-root');
 
+// this is a hack to give the angular SDK a model to export
+// TODO: create and inject a model the "proper" way
+var TestClass = {
+    name: 'TestClass',
+    sharedClass: {
+        ctor: {
+            settings: {
+                description: 'Test Class',
+            },
+        },
+    },
+    ctor: {
+        getFullPath: function() {}
+    },
+    methods: [],
+
+};
+
+app.models.TestClass = {
+    definition: {
+        properties: {
+            name: 'string',
+        },
+    },
+};
+
+app.handler('rest').adapter.getClasses = function() {
+    return [TestClass];
+};
+
 module.exports = app;
